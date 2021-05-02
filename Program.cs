8f876a41d40de18ea64c68241e5f23d56914a499
omersaskan <omerfaruksaskan@gmail.com>
2021-05-03 00:30:12 +0300

    igg

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                                    
+            int[] dizi = {1,3,2,11,5,22 };
+            int temp;
+            
+            for (int i = 0; i <dizi.Length; i++)
+            {
+                for (int j = 1; j <dizi.Length-1; j++)
+                {
+                    if (dizi[j]<dizi[j-1] && dizi[j-1]<dizi[j-2] )
+                    {
+                        temp = dizi[j - 2];
+                        dizi[j - 2] = dizi[j];
+                        dizi[j] = temp;
+
+
+                    }else if (dizi[j]<dizi[j-1] )
+                    {
+                        temp = dizi[j-1];
+                        dizi[j-1] = dizi[j];
+                        dizi[j] = temp;
+                    }
+                    
+                }
+            }
+
+            foreach (var dd in dizi)
+            {
+                Console.WriteLine(dd);
+            }
+        }
+    }
+}
